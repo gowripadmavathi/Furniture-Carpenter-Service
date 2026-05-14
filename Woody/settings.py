1812f6ba76187a5d8decb49f07a30bc7c319303a
Narala Gowri Padmavathi <gowripadmavathi111@gmail.com>
2026-05-14 10:30:03 +0530

    Update settings.py

--- Woody/settings.py
+++ Woody/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-apax_r9bhs@d$#(&p(g6+n$v(@cl*kuwgf6!j&t2xgc*-7a+kx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
