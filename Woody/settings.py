e504b4ac0acd1c3e80801d8aeb9b05857319748b
Narala Gowri Padmavathi <gowripadmavathi111@gmail.com>
2026-05-14 10:35:36 +0530

    Update settings.py

--- Woody/settings.py
+++ Woody/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+      'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Woody.urls'
@@ -133,3 +134,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL="login"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
